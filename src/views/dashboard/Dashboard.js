7b827f86f16bdae15112c4d1405be2d08f13bab8
Anthony <anthony.sarais@greencom-networks.com>
2018-06-14 16:04:49 +0200

    Dashboard, scaled up graph (1.0 to 1.5) and grouped nodes together

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -14,51 +14,79 @@ class Dashboard extends React.Component {
     componentDidMount() {
         BrokersService.getBrokers()
             .then(b => {
-                let nodes = null;
-                let edges = null;
+                    let nodes = null;
+                    let edges = null;
 
-                nodes = [];
-                edges = [];
+                    nodes = [];
+                    edges = [];
 
-                nodes.push({id: 'Cluster'});
+                    nodes.push({id: 'Cluster'});
 
-                let serializedZookeepers = [];
+                    let serializedZookeepers = [];
 
-                b.forEach(broker => {
-                    let zookeepers = broker.configurations["zookeeper.connect"].replace('/kafka', '').split(',');
-                    zookeepers.forEach(z => {
-                        let splitted = z.split(':');
-                        if (!serializedZookeepers.find(sz => sz.host === splitted[0] && sz.port === splitted[1])) {
-                            serializedZookeepers.push({
-                                id: z,
-                                host: splitted[0],
-                                port: splitted[1]
-                            })
+                    b.forEach(broker => {
+                        let zookeepers = broker.configurations["zookeeper.connect"].replace('/kafka', '').split(',');
+                        zookeepers.forEach(z => {
+                            let splitted = z.split(':');
+                            if (!serializedZookeepers.find(sz => sz.host === splitted[0] && sz.port === splitted[1])) {
+                                serializedZookeepers.push({
+                                    id: z,
+                                    host: splitted[0],
+                                    port: splitted[1]
+                                })
+                            }
+                        });
+
+                        let brokerInfo = `Broker ${broker.id}\nhost: ${broker.host}\nport: ${broker.port}`;
+                        nodes.push({
+                            id: "broker-" + broker.id,
+                            label: brokerInfo,
+                            image: kafkaImg,
+                            shape: 'image'
+                        });
+                        edges.push({from: "broker-" + broker.id, to: 'Cluster', length: 200});
+                    });
+
+                    serializedZookeepers.forEach(sz => {
+                        nodes.push({
+                            id: "zookeeper-" + sz.id,
+                            label: `Zookeeper \nhost: ${sz.host}\nport: ${sz.port}`,
+                            image: zookeeperImg,
+                            shape: 'image'
+                        });
+                        edges.push({from: "zookeeper-" + sz.id, to: 'Cluster', length: 200});
+                    });
+
+                    nodes.forEach(node1 => {
+                        nodes.forEach(node2 => {
+                            if (node1.id.includes('zookeeper') && node2.id.includes('zookeeper')
+                                || node1.id.includes('broker') && node2.id.includes('broker')) {
+                                let exist = edges.find(e => {
+                                    return (e.from === node1.id && e.to === node2.id)
+                                        || (e.from === node2.id && e.to === node1.id);
+                                });
+                                if (!exist && node1.id !== node2.id) {
+                                    edges.push({from: node1.id, to: node2.id, length: 200, color: {color: "rgba(0,0,0,0.08)"}});
+                                }
+                            }
+                        })
+                    });
+
+                    const vis = new Vis.Network(document.getElementById('visualization'), {nodes, edges}, {});
+                    vis.moveTo({
+                            scale: 1.5,
+                            animation: {
+                                duration: 1000000,
+                                easingFunction: "linear"
+                            }
                         }
-                    });
+                    )
+                }
+            );
+    }
 
-                    let brokerInfo = `Broker ${broker.id}\nhost: ${broker.host}\nport: ${broker.port}`;
-                    nodes.push({
-                        id: broker.id,
-                        label: brokerInfo,
-                        image: kafkaImg,
-                        shape: 'image'
-                    });
-                    edges.push({from: broker.id, to: 'Cluster', length: 200});
-                });
-
-                serializedZookeepers.forEach(sz => {
-                    nodes.push({
-                        id: sz.id,
-                        label: `Zookeeper \nhost: ${sz.host}\nport: ${sz.port}`,
-                        image: zookeeperImg,
-                        shape: 'image'
-                    });
-                    edges.push({from: sz.id, to: 'Cluster', length: 100});
-                });
-
-                new Vis.Network(document.getElementById('visualization'), {nodes, edges}, {});
-            });
+    getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
     render() {
@@ -70,6 +98,7 @@ class Dashboard extends React.Component {
                 <div className="box">
                     <div id="visualization"></div>
                 </div>
+
             </div>
         );
     }
